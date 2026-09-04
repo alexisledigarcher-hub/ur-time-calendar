963667a61e46a66d420f596b9d83c2e9340fcee1
alexisledigarcher-hub <alexis.ledigarcher@gmail.com>
2026-09-04 11:17:24 +0200

    Update getA26organization.ts

--- src/data/getA26organization.ts
+++ src/data/getA26organization.ts
@@ -1,10 +1,11 @@
-import { parseSemester } from "utils"
+
+
+import { parseSemester } from 'utils'
 
 const organisation = `
-
-ref: a26
-starts: 2026/08/31
-ends: 2027/01/16
+    ref: a25
+    starts:2026/08/26
+    ends:2027/01/31
     //Semaine de rentrée (31 août - 05 sept) : cours uniquement
     '2026/08/31-2026/08/31-x-x-x',
     '2026/09/01-2026/09/05-x-x-x',
